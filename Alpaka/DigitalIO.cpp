7bbdab61d1dcb7b1f948f7db269b8225b1c1a3c0
MrMoonshine <34010451+MrMoonshine@users.noreply.github.com>
2020-02-03 13:14:03 +0100

    Add files via upload

---
+++ Alpaka/DigitalIO.cpp
@@ -0,0 +1,61 @@
+//#include "sam.h"
+#include "DigitalIO.hpp"
+//#include <iostream>
+
+DigitalOut::DigitalOut(char reg_i, unsigned char pin_i,bool mode_i){
+    pin = pin_i;
+    mode = mode_i;
+    reg = reg_i;
+
+    if(reg == 'A' || reg == 'a'){
+        REG_PIOA_PER |= (1 << pin);   
+        REG_PIOA_OER |= (1 << pin); 
+    }
+    else if(reg == 'B' || reg == 'b'){
+        REG_PIOB_PER |= (1 << pin);
+        REG_PIOB_OER |= (1 << pin);  
+    }
+    else if(reg == 'C' || reg == 'c'){
+        REG_PIOC_PER |= (1 << pin);
+        REG_PIOC_OER |= (1 << pin); 
+    }
+    else if(reg == 'D' || reg == 'd'){
+        REG_PIOD_PER |= (1 << pin);
+        REG_PIOD_OER |= (1 << pin); 
+    }
+    else
+    return EXIT_FAILURE;
+}
+void DigitalOut::operator=(unsigned int x){
+    if(x & 1 == 1){
+        REG_PIOC_SODR |= (1 << pin); 
+    }else{
+        REG_PIOC_CODR |= (1 << pin)
+    }
+}
+
+void DigitalOut::operator!(){
+    if(read() == 0){
+        REG_PIOC_SODR |= (1 << pin); 
+    }else{
+        REG_PIOC_CODR |= (1 << pin)
+    }
+}
+
+int DigitalOut::read(){
+    if(reg == 'A' || reg == 'a')
+    return REG_PIOA_ODSR;
+    else if(reg == 'B' || reg == 'b')
+    return REG_PIOB_ODSR;
+    else if(reg == 'C' || reg == 'c')
+    return REG_PIOC_ODSR;
+    else if(reg == 'D' || reg == 'd')
+    return REG_PIOD_ODSR;
+}
+
+/*void DigitalOut::enablePull(bool dir){
+    ;
+}
+void DigitalOut::disablePull(bool dir){
+    ;
+}*/
---
+++ Alpaka/DigitalIO.hpp
@@ -0,0 +1,28 @@
+//#include "sam.h"
+
+#define OUTPUT true
+#define INPUT false
+#define UP true
+#define DOWN false
+
+class DigitalOut{
+    unsigned char pin;
+    char reg;
+    bool mode;
+public:
+    DigitalOut(char reg_i, unsigned char pin_i,bool mode_i);
+    void operator=(unsigned int x);
+    void operator!();
+    int read();
+};
+
+/*class DigitalIn{
+    unsigned char pin;
+    char reg;
+    bool mode;
+public:
+    DigitalIO(char reg_i, unsigned char pin_i,bool mode_i);
+    void enablePull(bool dir);
+    void disablePull(bool dir);
+    void operator=(unsigned int x);
+};*/
